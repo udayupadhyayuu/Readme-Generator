d01c10d806f7c4b21b0eaeb3e64cb83058f0d0ec
Uday Upadhyay <udayupadhyay.uu@gmail.com>
2025-09-09 13:43:34 +0000

    vite

--- github-readme-generator/vite.config.js
+++ github-readme-generator/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Readme-Generator/', // 👈 important for GitHub Pages
 })
